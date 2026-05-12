4573f6aa9a4cf6aedb495308fbb0603740412c15
haytham <haytham818@outlook.com>
2026-05-12 19:48:52 +0800

    some fix

--- lua/plugin/noice.lua
+++ lua/plugin/noice.lua
@@ -7,7 +7,7 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 	opts = {
 		--LSP
@@ -38,6 +38,10 @@ return {
 			long_message_to_split = true,
 			inc_rename = true,
 			lsp_doc_border = true,
+		},
+
+		cmdline = {
+			view = "cmdline",
 		},
 
 		routes = {
